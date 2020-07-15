e04e79275117e486976a7600678eb7ad9b944258
Kwonkunkun <59603575+Kwonkunkun@users.noreply.github.com>
2020-07-15 18:25:57 +0900

    수련님 맵작업 업로드 1/2

---
+++ Assets/FogMgr.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Aura2API;
+using UnityEditor;
+public class FogMgr : MonoBehaviour
+{
+    public bool isGripBow = false;
+    public bool isGripArrow = false;
+    public AuraCamera auraCamera;
+    void Start()
+    {
+    //GameObject camera =
+   // auraCamera.frustumSettings.
+        
+    }
+
+    void Update()
+    {
+        if(isGripBow && isGripArrow)
+        {
+            //auraCamera의 Scaterring 값을 서서히 줄인다
+            for(int i = 20; i>1; i-- )
+            //Scateriing 값
+            {
+                auraCamera.frustumSettings.BaseSettings.scattering = 0;
+            }
+        }
+    }
+}
