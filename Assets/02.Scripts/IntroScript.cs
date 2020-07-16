87e8a366156fdb1f207887c824dd889b177617d9
Kwonkunkun <59603575+Kwonkunkun@users.noreply.github.com>
2020-07-16 16:23:00 +0900

    Update intro

---
+++ Assets/02.Scripts/IntroScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Unity.Collections.LowLevel.Unsafe;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class IntroScript : MonoBehaviour
+{
+    public float timeOfPassScene;
+    private void Start()
+    {
+        StartCoroutine(PassScene());
+    }
+    public void ScenePass()
+    {
+        Debug.Log("PassScene");
+        StartCoroutine(PassScene());
+    }
+
+    IEnumerator PassScene()
+    { 
+        yield return new WaitForSeconds(timeOfPassScene);
+        SceneManager.LoadScene("RealMain");
+    }
+}
