d25c3f9df3be039278681fed5063e55c4a47faad
LILYSUMMER <64562629+LILYSUMMER@users.noreply.github.com>
2020-07-21 10:17:42 +0900

    Update Trail
    Arrow script 수정함

--- Assets/_BowAndArrow/Scripts/Arrow.cs
+++ Assets/_BowAndArrow/Scripts/Arrow.cs
@@ -8,10 +8,16 @@ public class Arrow : MonoBehaviour
     private Rigidbody m_Rigidbody = null;
     private bool m_IsStopped = true;
     private Vector3 m_LstPosition = Vector3.zero;
+    
+    //Trail 껐다 키기에 필요한 아이들
+    private TrailRenderer line;
+    
 
     private void Awake()
     {
         m_Rigidbody = GetComponent<Rigidbody>();
+
+        line = gameObject.GetComponent<TrailRenderer>();
     }
 
     private void FixedUpdate()
@@ -30,6 +36,8 @@ public class Arrow : MonoBehaviour
 
         m_Rigidbody.isKinematic = true;
         m_Rigidbody.useGravity = false;
+        
+       line.enabled = false;
     }
 
     public void Fire(float pulValue)
@@ -42,6 +50,8 @@ public class Arrow : MonoBehaviour
         m_Rigidbody.useGravity = true;
         m_Rigidbody.AddForce(transform.forward * (pulValue * m_Speed));
 
+        line.enabled = true;
+        
         Destroy(gameObject, 5.0f);
     }
 
